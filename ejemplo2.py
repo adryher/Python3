1d880c92a335c99395b1bac33b3e1dd1ef4b314b
adryher <adrianahernandez20010@gmail.com>
2019-01-30 16:54:16 -0600

    ejemplo de edad

---
+++ ejemplo2.py
@@ -0,0 +1,16 @@
+##adry
+##calcular la edad actual de una persona,
+#previamente ingresado
+##el año actual y el año de nacimiento
+print("bienvenido al preograma".center(50,"*"))
+FEC_ACT = 2019
+fec_nac = int(input("ingresa tu año de nacimiento"))
+
+edada = fec_nac
+print("tu edad es {}".format(edad))
+if edad >= 18:
+    print("Eres mayor de edad")
+else:
+    print("Eres menor de edad")
+
+
